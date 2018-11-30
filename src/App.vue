eef5410ca59bdb197711d85929839bc57d6c408d
Aleksander Giera <aleksander.giera@protonmail.com>
2018-11-30 17:47:20 +0100

    version one

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,31 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+
+      <div class="wrapper">
+      <div class="cont">
+        <div class="sideLeft"></div> 
+        <div class="main"><Main/></div>
+        <div class="sideRight"></div> 
+      </div>
+      </div>
+
     </v-content>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import Main from './components/Main'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Main
   },
   data () {
     return {
@@ -36,3 +34,33 @@ export default {
   }
 }
 </script>
+
+
+<style lang="scss" scoped>
+
+.wrapper{
+  justify-content: center;
+  display: flex;
+}
+
+  .cont{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: center;
+    width:90%;
+
+      .sideLeft, .sideRight{
+        width:10%;
+        margin-top: 50px;
+        // background: red;
+      }
+
+      .main{
+        width: 50%;
+        margin: 50px 20px 0 20px;
+        // background: lightblue;
+        min-height: 500px;
+      }
+  }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,69 @@
+<template>
+
+    <v-layout row>
+        <v-flex sm12 sm6>
+            <v-card>
+                <v-toolbar color="cyan" dark>
+                    <v-toolbar-title>To do List</v-toolbar-title>
+                    </v-toolbar>
+                    <v-list>
+                    <v-list-tile >
+                        <v-list-tile-content>
+                            <v-text-field color="cyan" light placeholder="Add new Task" class="inputText" v-model="inputText"></v-text-field>
+                        </v-list-tile-content>
+                        <v-list-tile-action>
+                            <v-btn @click="addNewTask(inputText)" color="cyan lighthen-2">Add</v-btn>
+                        </v-list-tile-action>
+                    </v-list-tile>
+                    <template v-for="task in taskList" :v-model="taskList">
+                    <v-divider :key="task"></v-divider>
+                    <v-list-tile :key="task">
+                        <v-list-tile-content>{{task.name}}</v-list-tile-content>
+                        <v-list-tile-action>
+                            <v-btn @click="deleteTask(task)" color="cyan darken-4">Done</v-btn>
+                        </v-list-tile-action>
+                    </v-list-tile>
+                    </template>
+                    </v-list>
+            </v-card>
+        </v-flex>
+    </v-layout>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            inputText: '',
+            taskList: [
+                {name: 'Task One'},
+                {name: 'Task Two'}
+            ]
+        }
+    },
+
+    methods:{
+        addNewTask(value) {
+            this.taskList.push({name: value});
+            this.inputText='';
+        },
+        deleteTask(task) {
+            this.taskList.splice(this.taskList.indexOf(task.name),1);
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    ul.taskList{
+        list-style-type: none;
+        padding: 10px;
+    }
+
+    .inputText{
+        width: 80%;
+        padding: 2px;
+    }
+</style>
+
